27c5e65c4c4d9cea1a48be0231b8faec90f7bb28
lizhaojun <lizhaojunleo@gmail.com>
2020-11-05 09:40:10 +0800

    ch05

---
+++ instructions/constants/nop.go
@@ -0,0 +1,10 @@
+package constants
+
+import "jvmgo/instructions/base"
+import "jvmgo/rtda"
+
+type NOP struct{ base.NoOperandsInstruction }
+
+func (self *NOP) Execute(frame *rtda.Frame) {
+	// do nothing
+}
