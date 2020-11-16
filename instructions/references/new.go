c0d693d5bbc8ed85b05d7a9b503c93857944cce3
lizhaojun <lizhaojunleo@gmail.com>
2020-11-16 19:32:29 +0800

    6.6.1

---
+++ instructions/references/new.go
@@ -0,0 +1,23 @@
+package references
+
+import "myjvm/instructions/base"
+import "myjvm/rtda"
+import "myjvm/rtda/heap"
+
+type NEW struct {
+	base.Index16Instruction
+}
+
+func (self *NEW) Execute(frame *rtda.Frame) {
+	cp := frame.Method().Class().ConstantPool()
+	classRef := cp.GetConstant(self.Index).(*heap.ClassRef)
+	class := classRef.ResolvedClass()
+	// todo: init class
+
+	if class.IsInterface() || class.IsAbstract() {
+		panic("java.lang.InstantiationError")
+	}
+
+	ref := class.NewObject()
+	frame.OperandStack().PushRef(ref)
+}
